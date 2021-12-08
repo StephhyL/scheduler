bc9803fd1d1ca39abeb8005f6ab6c7f2e45d9f5c
StephhyL <Stephanie Li>
2021-12-08 16:47:04 -0500

    add conditional rendering of appointments depending on time

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './styles.scss'
 
-const Appointment = (props) => {
+const Appointment = ({time}) => {
   return (
     <article className="appointment">
-
+      {!time && "No Appointments"}
+      {time && `Appointment at ${time}`}
     </article>
   )
 }
