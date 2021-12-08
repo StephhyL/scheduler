a81ced070fb1f13337d71981c18f25f846a76f06
StephhyL <Stephanie Li>
2021-12-07 20:57:08 -0500

    inital commit for InterviewerListItem

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const InterviewerListItem = () => {
+  return (
+    <li className="interviewes__item">
+      <img
+        className="interviewes__item-image"
+        src="https://i.imgur.com/LpaY82x.png"
+        alt="Sylvia Palmer"
+      />
+      Sylvia Palmer
+    </li>
+  )
+}
+
+export default InterviewerListItem
