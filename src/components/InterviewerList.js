16205111f201027a640010c43a0fbf79459fd152
StephhyL <Stephanie Li>
2021-12-07 22:32:04 -0500

    change condition for selected

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,10 +3,27 @@ import InterviewerListItem from './InterviewerListItem';
 import "components/InterviewerList.scss";
 
 const InterviewerList = ({interviewers, setInterviewer, interviewer}) => {
+
+  console.log("interviewers--->", interviewers)
+
+  const parsedInterviewer = interviewers.map((interviewerObj) =>
+    <InterviewerListItem 
+      key = {interviewerObj.id}
+      id = {interviewerObj.id}
+      name = {interviewerObj.name}
+      avatar= {interviewerObj.avatar}
+      selected= {interviewerObj.id === interviewer}
+      setInterviewer={setInterviewer}
+    />
+  )
+
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {parsedInterviewer}
+      </ul>
     </section>
   )
 }
