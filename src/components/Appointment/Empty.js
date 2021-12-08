2acb7e873e6db7e247e5cbb99de7fa1843fe99ea
StephhyL <Stephanie Li>
2021-12-08 17:22:12 -0500

    add in prop of onAdd and use it as part of onClick

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const Empty = () => {
+const Empty = ({onAdd}) => {
   return (
     <main className="appointment__add">
       <img
         className="appointment__add-button"
         src="images/add.png"
         alt="Add"
+        onClick={onAdd}
       />
     </main>
   )
