d8d2c0bf30432c808ace310bfe971497d7d96ef5
StephhyL <Stephanie Li>
2021-12-08 17:09:11 -0500

    correct import for Header.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './styles.scss'
+import Header from './Header'
 
 const Appointment = ({time}) => {
   return (
     <article className="appointment">
+      <Header />
       {!time && "No Appointments"}
       {time && `Appointment at ${time}`}
     </article>
