2c5774a521e3335776a625196e9d98e9402f437e
StephhyL <Stephanie Li>
2021-12-07 20:44:22 -0500

    add conditional class names based on props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,36 @@
 import React from "react";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions/dist/preview";
+import className from 'classnames';
 
 export default function Button(props) {
-   return <></>;
+   
+   
+   let buttonClass = className(
+      "button",
+      {"button--confirm": props.confirm,
+      "button--danger": props.danger}
+   )
+   
+   // let buttonClass = "button";
+   
+
+   // if(props.confirm) {
+   //    buttonClass += " button--confirm"
+   // }
+
+   // if(props.danger) {
+   //    buttonClass += " button--danger"
+   // }
+
+   return (
+      <button 
+         className={buttonClass} 
+         onClick={props.onClick} 
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+      );
 }
