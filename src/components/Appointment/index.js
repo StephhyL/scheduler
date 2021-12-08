b70e200ad10221267620fe02c1eecb51387472fa
StephhyL <Stephanie Li>
2021-12-08 17:22:52 -0500

    import Empty component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './styles.scss'
 import Header from './Header'
+import Empty from './Empty'
 
 const Appointment = ({time}) => {
   return (
     <article className="appointment">
       <Header />
-      {!time && "No Appointments"}
-      {time && `Appointment at ${time}`}
+      <Empty />
     </article>
   )
 }
