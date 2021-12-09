a24896a7eab91c127c2a792367f50bef3deb820d
StephhyL <Stephanie Li>
2021-12-08 20:13:41 -0500

    short circuit condition for displaying Header if time is true, and ternary operator for displaying Show or Empty component depending on interview

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import './styles.scss'
 import Header from './Header'
 import Empty from './Empty'
@@ -7,11 +7,13 @@ import Confirm from './Confirm'
 import Status from './Status'
 import Form from './Form'
 
-const Appointment = ({time}) => {
+const Appointment = ({time, interview}) => {
   return (
     <article className="appointment">
-      <Header />
-      <Empty />
+      {time && <Header time={time}/>}
+      {interview ? <Show /> : <Empty />}
+      {/* <Empty />
+      <Show /> */}
     </article>
   )
 }
