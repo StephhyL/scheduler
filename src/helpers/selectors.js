b2f00def5a3b52306754e9ec20b7b88a79f48fce
StephhyL <Stephanie Li>
2021-12-09 18:30:50 -0500

    code for getAppointmentsForDay function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,19 @@
+export const getAppointmentsForDay = (state, day) => {
+
+  
+  const filteredState = state.days.filter(dayObj => dayObj.name === day); 
+  console.log("filtered State---->", filteredState);
+
+  if (!state.days.length || !filteredState.length) {
+    return [];
+  }
+
+  const apptIdArrForDay = filteredState[0].appointments // array of appointments [1,2,3]
+
+  // console.log("apptIdArrForDay---------->", apptIdArrForDay)
+
+  const apptArrForDay = apptIdArrForDay.map((element) => state. appointments[element])
+
+  console.log(apptArrForDay);
+  return apptArrForDay;
+};
