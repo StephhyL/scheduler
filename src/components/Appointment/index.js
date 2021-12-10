20aa1efc3e0b4c0074e8e88aca1ea01142174a64
StephhyL <Stephanie Li>
2021-12-10 18:14:33 -0500

    import Form and conditional rendering of FORM display and implement onCancel function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,15 +3,36 @@ import './styles.scss'
 import Header from './Header'
 import Empty from './Empty'
 import Show from './Show'
+import useVisualMode from 'hooks/useVisualMode'
+import Form from './Form'
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
 
 const Appointment = ({time, interview}) => {
-  console.log("interview-in index--->", interview)
+  const {mode, transition, back} = useVisualMode(interview? SHOW : EMPTY)
+
+  console.log(mode);
+
   return (
     <article className="appointment">
       {time && <Header time={time}/>}
-      {interview ? 
-        <Show student={interview.student} interviewer={interview.interviewer.name}/> :
-         <Empty />}
+      {mode === EMPTY && (
+       <Empty 
+        onAdd={() => {transition(CREATE)}}
+       />)}
+      {mode === SHOW && (
+        <Show 
+          student={interview.student} 
+          interviewer={interview.interviewer.name}
+          />) }
+      {mode === CREATE && (
+        <Form 
+          interviewers={[]}
+          onCancel = {()=>{back()}}
+        />
+      )}
     </article>
   )
 }
