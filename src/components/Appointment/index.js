015d71888c7e9147d1098e834b2bd5980abef284
StephhyL <Stephanie Li>
2021-12-10 03:32:45 -0500

    remove unnecessary imports and comment out console.log

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,13 +1,11 @@
-import React, {Fragment} from 'react'
+import React from 'react'
 import './styles.scss'
 import Header from './Header'
 import Empty from './Empty'
 import Show from './Show'
-import Confirm from './Confirm'
-import Status from './Status'
-import Form from './Form'
 
 const Appointment = ({time, interview}) => {
+  // console.log("interview-in index--->", interview)
   return (
     <article className="appointment">
       {time && <Header time={time}/>}
