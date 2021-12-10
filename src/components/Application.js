2025ad80acf951e41cb3af026b5868139aed4e8b
StephhyL <Stephanie Li>
2021-12-09 19:15:28 -0500

    comment out hard coded appointments, add appointments to state, add function to setState so it depends on prev state

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,45 +6,53 @@ import axios from "axios";
 
 import "components/Application.scss";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm"
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82.png"
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.png"
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm"
-  }
-]
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm"
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82.png"
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer: {
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.png"
+//       }
+//     }
+//   },
+//   {
+//     id: 5,
+//     time: "4pm"
+//   }
+// ]
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "",
+    days: [],
+    appointments: {}
+  })
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+
+  const setDay = day => setState(prev => ({...prev, day}))
+  const setDays = days => setState(prev => ({...prev, days}))
 
   // const days = [
   //   {
@@ -73,7 +81,6 @@ export default function Application(props) {
   })
 
   useEffect(() => {
-
     let urlDays = `/api/days`;
     axios.get(urlDays)
       .then((response) => {
@@ -95,9 +102,9 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList 
-            days={days}
-            value={day}
-            onChange={setDay}
+            days={state.days}
+            value={state.day}
+            onChange={setDay} //********* */
           />
         </nav>
         <img
