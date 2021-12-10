78912f952ffe709e4f87991b2f4654aea067314e
StephhyL <Stephanie Li>
2021-12-10 03:55:58 -0500

    implement history array, adjust transition function, implement back function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,12 +2,25 @@ import { useState } from "react";
 
 const useVisualMode = (initalMode) => {
   const [mode, setMode] = useState(initalMode);
+  const [history, setHistory] = useState([initalMode]);
+
+  console.log("HISTORY OUTSIDE---->", history)
 
   const transition = (newMode) => {
+    // history.push(newMode);
+    // console.log("history inside transition-->", history)
+    setHistory([...history, newMode]);
     setMode(newMode);
   }
+
+  const back = () => {
+    history.pop();
+    // console.log("history--->", history)
+    let prevMode = history[history.length - 1];
+    setMode(prevMode);
+  };
   
-  return {mode, transition};
+  return {mode, transition, back};
 };
 
 
