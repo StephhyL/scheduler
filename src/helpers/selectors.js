5671714225618bd4b21d2a7126d5cd905e93f8ce
StephhyL <Stephanie Li>
2021-12-10 18:57:18 -0500

    add getInterviewersForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -19,6 +19,35 @@ export const getAppointmentsForDay = (state, day) => {
   return apptArrForDay;
 };
 
+//*** getInterviewerByDay */
+export const getInterviewersForDay = (state, day) => {
+
+  const filteredState = state.days.filter(dayObj => dayObj.name === day); 
+  // console.log("filtered State---->", filteredState);
+
+  if (!state.days.length || !filteredState.length) {
+    // console.log("state---->", state)
+    return [];
+  }
+
+  const interviewersIdArrForDay = filteredState[0].interviewers // array of appointments [1,2,3]
+
+  // console.log("apptIdArrForDay---------->", apptIdArrForDay)
+
+  const interviewersArrForDay = interviewersIdArrForDay.map((element) => state.interviewers[element])
+
+  // console.log(apptArrForDay);
+  // console.log("state---->", state)
+  console.log("interviewersArrForDay---->", interviewersArrForDay)
+  return interviewersArrForDay;
+};
+
+
+
+
+
+
+//***** getInterview */
 export const getInterview = (state, interview) => {
   
   if (!interview) {
