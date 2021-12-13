37351028a733aec23b11442b6dc872af89060971
StephhyL <Stephanie Li>
2021-12-12 20:23:26 -0500

    remove unnecessary comments

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,28 +1,14 @@
 import React from "react";
 
 import "components/Button.scss";
-import { action } from "@storybook/addon-actions/dist/preview";
 import className from 'classnames';
 
 export default function Button(props) {
-   
-   
    let buttonClass = className(
       "button",
       {"button--confirm": props.confirm,
       "button--danger": props.danger}
    )
-   
-   // let buttonClass = "button";
-   
-
-   // if(props.confirm) {
-   //    buttonClass += " button--confirm"
-   // }
-
-   // if(props.danger) {
-   //    buttonClass += " button--danger"
-   // }
 
    return (
       <button 
