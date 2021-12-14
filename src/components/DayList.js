1347a238ad0f5e592af63ed6dc61623f0477de23
StephhyL <Stephanie Li>
2021-12-14 18:02:29 -0500

    remove comments

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,31 +1,20 @@
-
 import React from "react";
 import DayListItem from "./DayListItem";
 
-const DayList = ({days, value, onChange}) => {
+const DayList = ({ days, value, onChange }) => {
+  const parsedDays = days.map((dayObj) => {
+    return (
+      <DayListItem
+        key={dayObj.id}
+        name={dayObj.name}
+        spots={dayObj.spots}
+        selected={dayObj.name === value}
+        setDay={onChange}
+      />
+    );
+  });
 
+  return <ul>{parsedDays}</ul>;
+};
 
-
-  const parsedDays = days.map((dayObj) => 
-  {
-    // console.log("dayObj---->", dayObj)
-    // console.log("onChange--->", onChange)
-    return <DayListItem 
-      key={dayObj.id}
-      name={dayObj.name}
-      spots={dayObj.spots}
-      selected={dayObj.name === value}
-      setDay={onChange}
-      // setDay= {day => console.log(day)}
-      /> })
-
-  // console.log("parsedDays----->,", parsedDays)
-
-  return (
-    <ul>
-      {parsedDays}
-    </ul>
-  )
-}
-
-export default DayList
+export default DayList;
