b1003cbd39057b18e581b04736a1e56b562603f6
StephhyL <Stephanie Li>
2021-12-14 06:55:53 -0500

    import PropTypes, have props as parameter, destructure props in InterviewerList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,30 +1,26 @@
-import React from 'react';
-import InterviewerListItem from './InterviewerListItem';
+import React, { PropTypes } from "react";
+import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
 
-const InterviewerList = ({interviewers, onChange, value}) => {
+const InterviewerList = (props) => {
+  const { interviewers, onChange, value } = props;
 
-  // console.log("interviewers--->", interviewers)
-
-  const parsedInterviewer = interviewers.map((interviewerObj) =>
-    <InterviewerListItem 
-      key = {interviewerObj.id}
-      name = {interviewerObj.name}
-      avatar= {interviewerObj.avatar}
-      selected= {interviewerObj.id === value}
+  const parsedInterviewer = interviewers.map((interviewerObj) => (
+    <InterviewerListItem
+      key={interviewerObj.id}
+      name={interviewerObj.name}
+      avatar={interviewerObj.avatar}
+      selected={interviewerObj.id === value}
       setInterviewer={() => onChange(interviewerObj.id)}
     />
-  )
-
+  ));
 
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">
-        {parsedInterviewer}
-      </ul>
+      <ul className="interviewers__list">{parsedInterviewer}</ul>
     </section>
-  )
-}
+  );
+};
 
-export default InterviewerList
+export default InterviewerList;
