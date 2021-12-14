195af3366458b4b33b38b079c64537b2231d6118
StephhyL <Stephanie Li>
2021-12-14 03:46:00 -0500

    add comment for overall hook function

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+/**Hook responsible for inital render of page and subsequent renderings. Responsible for passing data to other components*/
 export const useApplicationData = () => {
   const [state, setState] = useState({
     day: "",
