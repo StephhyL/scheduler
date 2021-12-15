b27def6c467ef6ec6c4141a407fb79c131d253bd
StephhyL <Stephanie Li>
2021-12-15 13:14:02 -0500

    add comments for the uses for the selectors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,58 +1,49 @@
+/** returns an array with appointment objects for a given state and day*/
 export const getAppointmentsForDay = (state, day) => {
+  // get an array with the day object where each day matches the target day
   const filteredState = state.days.filter((dayObj) => dayObj.name === day);
-  // console.log("filtered State---->", filteredState);
 
   if (!state.days.length || !filteredState.length) {
-    // console.log("state---->", state)
     return [];
   }
 
-  const apptIdArrForDay = filteredState[0].appointments; // array of appointments [1,2,3]
-
-  // console.log("apptIdArrForDay---------->", apptIdArrForDay)
-
+  // grab the appt id array of from the day object in the filteredState array
+  const apptIdArrForDay = filteredState[0].appointments;
+  // a new array with appt objects that correspond to the appt ids
   const apptArrForDay = apptIdArrForDay.map(
     (element) => state.appointments[element]
   );
 
-  // console.log(apptArrForDay);
-  // console.log("state---->", state)
   return apptArrForDay;
 };
 
-//*** getInterviewerByDay */
+/** returns an array with interviewer objects for a given state and day */
 export const getInterviewersForDay = (state, day) => {
+  // get an array with the day object where each day matches the target day
   const filteredState = state.days.filter((dayObj) => dayObj.name === day);
-  // console.log("filtered State---->", filteredState);
 
   if (!state.days.length || !filteredState.length) {
-    // console.log("state---->", state)
     return [];
   }
 
-  const interviewersIdArrForDay = filteredState[0].interviewers; // array of appointments [1,2,3]
-
-  // console.log("apptIdArrForDay---------->", apptIdArrForDay)
-
+  // grab the interviewers id array of from the day object in the filteredState array
+  const interviewersIdArrForDay = filteredState[0].interviewers;
+  // a new array with appt objects that correspond to the interviewers ids
   const interviewersArrForDay = interviewersIdArrForDay.map(
     (element) => state.interviewers[element]
   );
 
-  // console.log(apptArrForDay);
-  // console.log("state---->", state)
-  // console.log("interviewersArrForDay---->", interviewersArrForDay)
   return interviewersArrForDay;
 };
 
-//***** getInterview */
+/** given a state and interview, returns an object with the interview where the interviewer key's value now includes the interviewer's name, id, avatar*/
 export const getInterview = (state, interview) => {
   if (!interview) {
     return null;
   }
 
   const interviewerId = interview.interviewer;
-
-  let newInterview = {
+  const newInterview = {
     ...interview,
     interviewer: state.interviewers[interviewerId],
   };
