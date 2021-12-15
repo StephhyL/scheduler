7cab46ee13b3eb86b43ce84787b947757a28fb8a
StephhyL <Stephanie Li>
2021-12-15 14:37:09 -0500

    add steps to type student name in specific input field

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -2,5 +2,11 @@ describe("Appointments", () => {
   it("should book an interview", () => {
     cy.visit("/");
     cy.contains("Monday");
+
+    cy.get("[alt=Add]").first().click();
+
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones", {
+      delay: 150,
+    });
   });
 });
