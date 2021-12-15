40a70ef900ec045e93df246fb838860de82af52d
StephhyL <Stephanie Li>
2021-12-14 21:56:52 -0500

    linted

--- src/helpers/__tests__/selectors.test.js
+++ src/helpers/__tests__/selectors.test.js
@@ -1,4 +1,4 @@
-import {getAppointmentsForDay} from "helpers/selectors";
+import { getAppointmentsForDay } from "helpers/selectors";
 import { getInterviewersForDay } from "helpers/selectors";
 import { getInterview } from "helpers/selectors";
 
@@ -7,54 +7,54 @@ const state = {
     {
       id: 1,
       name: "Monday",
-      appointments: [1,2,3],
-      interviewers:[1,2]
+      appointments: [1, 2, 3],
+      interviewers: [1, 2],
     },
     {
       id: 2,
       name: "Tuesday",
-      appointments: [4,5],
-      interviewers:[2, 3]
+      appointments: [4, 5],
+      interviewers: [2, 3],
     },
   ],
   appointments: {
-    "1": {id: 1, time: "12pm", interview: null},
-    "2": {id: 2, time: "1pm", interview: null},
-    "3": {
-      id: 3, 
+    1: { id: 1, time: "12pm", interview: null },
+    2: { id: 2, time: "1pm", interview: null },
+    3: {
+      id: 3,
       time: "2pm",
       interview: {
         student: "Archie Cohen",
-        interviewer: 2
-      }
+        interviewer: 2,
+      },
     },
-    "4": {id: 4, time: "3pm", interview: null},
-    "5": {
-      id: 5, 
+    4: { id: 4, time: "3pm", interview: null },
+    5: {
+      id: 5,
       time: "4pm",
       interview: {
         student: "Chad Takahasi",
-        interviewer: 2
-      }
-    }
+        interviewer: 2,
+      },
+    },
   },
   interviewers: {
-    "1": {
-      "id": 1,
-      "name":"Sylvia Palmer",
-      "avatar": "https://i.imgur.com/LpaY82x.png"
+    1: {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png",
     },
-    "2": {
-      "id": 2,
-      "name": "Tori Malcolm",
-      "avatar": "https://i.imgur.com/Nmx0Qxo.png"
+    2: {
+      id: 2,
+      name: "Tori Malcolm",
+      avatar: "https://i.imgur.com/Nmx0Qxo.png",
     },
-    "3": {
-      "id": 3,
-      "name": "Mildred Nazir",
-      "avatar": "https://i.imgur.com/T2WwVfS.png"
-    }
-  }
+    3: {
+      id: 3,
+      name: "Mildred Nazir",
+      avatar: "https://i.imgur.com/T2WwVfS.png",
+    },
+  },
 };
 
 test("getAppointmentsForDay returns an array", () => {
@@ -67,14 +67,14 @@ test("getAppointmentsForDay returns an array with a length matching the number o
   expect(result.length).toEqual(3);
 });
 
-test("getAppointmentsForDay returns an array containing the correct appointment objects", ()=> {
+test("getAppointmentsForDay returns an array containing the correct appointment objects", () => {
   const [first, second] = getAppointmentsForDay(state, "Tuesday");
   expect(first).toEqual(state.appointments["4"]);
   expect(second).toEqual(state.appointments["5"]);
 });
 
 test("getAppointmentsForDay returns an empty array when the days data is empty", () => {
-  const result = getAppointmentsForDay({days:[]}, "Monday");
+  const result = getAppointmentsForDay({ days: [] }, "Monday");
   expect(result.length).toEqual(0);
 });
 
@@ -93,14 +93,14 @@ test("getInterviewersForDay returns an array with a length matching the number o
   expect(result.length).toEqual(2);
 });
 
-test("getInterviewersForDay returns an array containing the correct appointment objects", ()=> {
+test("getInterviewersForDay returns an array containing the correct appointment objects", () => {
   const [first, second] = getInterviewersForDay(state, "Tuesday");
   expect(first).toEqual(state.interviewers["2"]);
   expect(second).toEqual(state.interviewers["3"]);
 });
 
 test("getInterviewersForDay returns an empty array when the days data is empty", () => {
-  const result = getInterviewersForDay({days:[]}, "Monday");
+  const result = getInterviewersForDay({ days: [] }, "Monday");
   expect(result.length).toEqual(0);
 });
 
@@ -109,19 +109,17 @@ test("getInterviewersForDay returns an empty array when the day is not found", (
   expect(result.length).toEqual(0);
 });
 
-
-
 ////***** test for getInterview */
 test("getInterview returns an object with the interviewer data", () => {
   const result = getInterview(state, state.appointments["3"].interview);
   expect(result).toEqual(
     expect.objectContaining({
-      student:expect.any(String),
+      student: expect.any(String),
       interviewer: expect.objectContaining({
         id: expect.any(Number),
         name: expect.any(String),
-        avatar: expect.any(String)
-      })
+        avatar: expect.any(String),
+      }),
     })
   );
 });
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,6 +1,5 @@
 export const getAppointmentsForDay = (state, day) => {
-
-  const filteredState = state.days.filter(dayObj => dayObj.name === day); 
+  const filteredState = state.days.filter((dayObj) => dayObj.name === day);
   // console.log("filtered State---->", filteredState);
 
   if (!state.days.length || !filteredState.length) {
@@ -8,11 +7,13 @@ export const getAppointmentsForDay = (state, day) => {
     return [];
   }
 
-  const apptIdArrForDay = filteredState[0].appointments // array of appointments [1,2,3]
+  const apptIdArrForDay = filteredState[0].appointments; // array of appointments [1,2,3]
 
   // console.log("apptIdArrForDay---------->", apptIdArrForDay)
 
-  const apptArrForDay = apptIdArrForDay.map((element) => state.appointments[element])
+  const apptArrForDay = apptIdArrForDay.map(
+    (element) => state.appointments[element]
+  );
 
   // console.log(apptArrForDay);
   // console.log("state---->", state)
@@ -21,8 +22,7 @@ export const getAppointmentsForDay = (state, day) => {
 
 //*** getInterviewerByDay */
 export const getInterviewersForDay = (state, day) => {
-
-  const filteredState = state.days.filter(dayObj => dayObj.name === day); 
+  const filteredState = state.days.filter((dayObj) => dayObj.name === day);
   // console.log("filtered State---->", filteredState);
 
   if (!state.days.length || !filteredState.length) {
@@ -30,11 +30,13 @@ export const getInterviewersForDay = (state, day) => {
     return [];
   }
 
-  const interviewersIdArrForDay = filteredState[0].interviewers // array of appointments [1,2,3]
+  const interviewersIdArrForDay = filteredState[0].interviewers; // array of appointments [1,2,3]
 
   // console.log("apptIdArrForDay---------->", apptIdArrForDay)
 
-  const interviewersArrForDay = interviewersIdArrForDay.map((element) => state.interviewers[element])
+  const interviewersArrForDay = interviewersIdArrForDay.map(
+    (element) => state.interviewers[element]
+  );
 
   // console.log(apptArrForDay);
   // console.log("state---->", state)
@@ -42,18 +44,18 @@ export const getInterviewersForDay = (state, day) => {
   return interviewersArrForDay;
 };
 
-
-
 //***** getInterview */
 export const getInterview = (state, interview) => {
-  
   if (!interview) {
     return null;
   }
 
   const interviewerId = interview.interviewer;
-  
-  let newInterview = {...interview, interviewer: state.interviewers[interviewerId]}
+
+  let newInterview = {
+    ...interview,
+    interviewer: state.interviewers[interviewerId],
+  };
 
   return newInterview;
 };
