6521d3e9f2d33028b6999eebe170ba1de5d58e00
StephhyL <Stephanie Li>
2021-12-15 00:42:36 -0500

    add queries to find Tuesday and click, and check css

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,4 +2,13 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+
+    cy.get("li")
+      .contains("Tuesday")
+      .click()
+      .should("have.css", "background-color", "rbg(242,242,242)");
+  });
 });
