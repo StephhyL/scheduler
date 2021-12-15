caaa84a2fab7e370e1a973eaf444cdf043e5e4fb
StephhyL <Stephanie Li>
2021-12-15 00:46:28 -0500

    correct should query to rgb

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -6,9 +6,8 @@ describe("Navigation", () => {
   it("should navigate to Tuesday", () => {
     cy.visit("/");
 
-    cy.get("li")
-      .contains("Tuesday")
+    cy.contains("li", "Tuesday")
       .click()
-      .should("have.css", "background-color", "rbg(242,242,242)");
+      .should("have.css", "background-color", "rgb(242, 242, 242)");
   });
 });
